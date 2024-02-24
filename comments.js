799b8778145505ebb2b08d5404dd883f3178c6cd
Andrés <mail.andresp@gmail.com>
2024-02-24 23:54:11 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,27 @@
-// Create a web server
+// Create web server
+// Usage: node comments.js
+// Test with curl -X POST -d "id=5&comment=hello" http://localhost:8080/comments
 
-const express = require('express');
-const app = express();
-const comments = require('./comments.json');
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+var comments = [];
 
-app.use(express.static('public'));
-
-app.get('/comments', (req, res) => {
-  res.json(comments);
-});
-
-app.listen(3000, () => {
-  console.log('Server is up and running on port 3000');
-});
+http.createServer(function (req, res) {
+  var url_parts = url.parse(req.url);
+  if (req.method == 'POST') {
+    var body = '';
+    req.on('data', function (data) {
+      body += data;
+    });
+    req.on('end', function () {
+      var post = querystring.parse(body);
+      comments.push(post);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Comment added\n');
+    });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(JSON.stringify(comments) + '\n');
+  }
+}).listen(8080, 'localhost');
