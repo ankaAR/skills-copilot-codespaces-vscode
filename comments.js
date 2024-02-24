276c4f81c58fcbc1fa5c2ed98f41b57417d3ff06
Andrés <mail.andresp@gmail.com>
2024-02-24 23:51:02 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -13,38 +13,3 @@ app.get('/comments', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is up and running on port 3000');
 });
-
-// Path: public/index.html
-// Create a web page
-
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Comments</title>
-  </head>
-  <body>
-    <h1>Comments</h1>
-    <ul id="comments"></ul>
-    <script>
-      fetch('/comments')
-        .then(response => response.json())
-        .then(comments => {
-          const commentsList = document.getElementById('comments');
-          comments.forEach(comment => {
-            const li = document.createElement('li');
-            li.innerText = comment.name;
-            commentsList.appendChild(li);
-          });
-        });
-    </script>
-  </body>
-</html>
-
-// Path: comments.json
-// Create a JSON file
-
-[
-  {
-    "id": 1,
-    "name": "John Doe",
-    "email": "
